b1802a53bf6ec071a30e8b52339c56393e6cb152
Ionut Traistaru <ionut.m.traistaru@gmail.com>
2019-06-14 00:40:10 +0100

    create home page

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
+/**
+ * Home page component
+ */
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  /**
+   * Redirect user to sign up page
+   */
+  onSignUp() {
+    this.router.navigate(['/user/signup']);
   }
 
+  /**
+   * Redirect user to log in page
+   */
+  onLogIn() {
+    this.router.navigate(['/user/login']);
+  }
 }
