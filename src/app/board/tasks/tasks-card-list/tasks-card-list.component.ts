97f38360d86966084baea685654fabecd849e9f1
Ionut Traistaru <ionut.m.traistaru@gmail.com>
2019-11-28 18:31:46 +0000

    undo status

--- src/app/board/tasks/tasks-card-list/tasks-card-list.component.ts
+++ src/app/board/tasks/tasks-card-list/tasks-card-list.component.ts
@@ -1,22 +1,24 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { ITask } from '../../task.model';
 import { TaskService } from '../../task.service';
+import { UIService } from '../../../shared/ui.service';
 
 @Component({
   selector: 'app-tasks-card-list',
   templateUrl: './tasks-card-list.component.html',
   styleUrls: ['./tasks-card-list.component.css']
 })
-export class TasksCardListComponent implements OnInit {
+export class TasksCardListComponent {
   @Input() tasks: ITask[];
   @Input() title: string;
-  @Input() tooltip: string;
+  @Input() forwardTooltip: string;
+  @Input() backTooltip: string;
   @Output() delete = new EventEmitter<void>();
   @Output() changeStatus = new EventEmitter<void>();
+  private forward = false;
+  private backward = false;
 
-  constructor(private taskService: TaskService) {}
-
-  ngOnInit() {}
+  constructor(private taskService: TaskService, private uiService: UIService) {}
 
   /**
    * Delete task.
@@ -28,24 +30,48 @@ export class TasksCardListComponent implements OnInit {
   }
 
   /**
+   * Change status on forward.
+   * @param id -> task id.
+   */
+  onForward(id: string) {
+    this.backward = false;
+    this.forward = true;
+    this.onChangeStatus(id);
+  }
+
+  /**
+   * Change status on backward.
+   * @param id -> task id.
+   */
+  onBackward(id: string) {
+    this.forward = false;
+    this.backward = true;
+    this.onChangeStatus(id);
+  }
+
+  /**
    * Change status.
    * @param id -> task id.
    * @returns promise
    */
-  async onChangeStatus(id: string): Promise<void> {
+  private async onChangeStatus(id: string): Promise<void> {
     try {
-      const result = await this.taskService.getMappedTask(id).toPromise();
+      const task = await this.taskService.getMappedTask(id).toPromise();
+
       let status: 'todo' | 'doing' | 'done';
-      if (result.status.includes('todo')) {
-        status = 'doing';
-      } else if (result.status.includes('doing')) {
-        status = 'done';
+
+      if (this.forward) {
+        status = task.status.includes('todo') ? 'doing' : 'done';
+      }
+
+      if (this.backward) {
+        status = task.status.includes('done') ? 'doing' : 'todo';
       }
 
       this.taskService.updateTask(id, { status }).subscribe();
       return this.changeStatus.emit();
     } catch (error) {
-      return console.log(error.message);
+      this.uiService.showSnackBar('Something went wrong! Try again later.', null, 5000, 'top');
     }
   }
 }
